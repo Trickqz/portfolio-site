af951d53b7600b95883d4be2d7abab90d4edaaff
Patrick Tavares <opatricktavares@gmail.com>
2024-08-05 11:48:17 -0300

    fix variant badge

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -11,9 +11,9 @@ export interface BadgeProps
   extends React.HTMLAttributes<HTMLDivElement>,
     VariantProps<typeof badgeVariants> {}
 
-function Badge({ className, variant, ...props }: BadgeProps) {
+function Badge({ className, ...props }: BadgeProps) {
   return (
-    <div className={cn(badgeVariants({ variant }), className)} {...props} />
+    <div className={cn(badgeVariants({  }), className)} {...props} />
   )
 }
 
